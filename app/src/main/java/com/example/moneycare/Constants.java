d12c6fff7301b1ec6a25fc6b6e8311235313eb4c
siva-krish24 <sivakrishna.guntreddi@gmail.com>
2022-10-16 19:39:50 +0530

    updated IP

--- app/src/main/java/com/example/moneycare/Constants.java
+++ app/src/main/java/com/example/moneycare/Constants.java
@@ -2,7 +2,7 @@ package com.example.moneycare;
 
 public class Constants {
     public static final String USER_EMAIL = "userEmail";
-    public static final String HOSTNAME = "10.0.2.2";
+    public static final String HOSTNAME = "43.205.231.224";
     public static  final  String EMPTY_STR = "";
     public static final int AMOUNT = 50;
 
